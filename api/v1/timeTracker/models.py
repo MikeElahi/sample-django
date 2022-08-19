5101e2dbe86bcd761b52b9e182022f504cf32258
Mohammad H. Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-08-20 01:50:20 +0430

    feat: add Project & TimeLog models

--- api/v1/timeTracker/models.py
+++ api/v1/timeTracker/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    title = models.TextField(null=False, help_text="Required, Title of your project")
+    slug = models.SlugField()
+    users = models.ManyToManyField(User)
+
+class TimeLog(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_at = models.DateTimeField(null=True)
+    finish_at = models.DateTimeField(null=True)
+    duration = models.IntegerField(null=True)

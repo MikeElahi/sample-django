992d01b0bae7f250f32419c4b75153c8c93100ad
Mohammad H. Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-08-21 01:20:12 +0430

    feat: add auth path

--- api/v1/timeTracker/urls.py
+++ api/v1/timeTracker/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.authtoken import views
 from .views import ProjectViewSet, UserViewSet, TimeLogViewSet
 
 router = routers.DefaultRouter()
@@ -9,5 +10,6 @@ router.register(r'timelogs', TimeLogViewSet, basename='timelog')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'django_nose',
     'api.v1.timeTracker.apps.TimetrackerConfig',
 ]
@@ -127,7 +128,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 # Use nose to run all tests

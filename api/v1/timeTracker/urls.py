8c249248a9b3e037ff547958d3c1bb2d51daf0aa
Mohammad H. Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-08-21 01:28:32 +0430

    fix: users path is used for debugging

--- api/v1/timeTracker/urls.py
+++ api/v1/timeTracker/urls.py
@@ -1,12 +1,16 @@
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken import views
+
+from config.settings import DEBUG
 from .views import ProjectViewSet, UserViewSet, TimeLogViewSet
 
 router = routers.DefaultRouter()
 router.register(r'projects', ProjectViewSet, basename='project')
-router.register(r'users', UserViewSet, basename='user')
 router.register(r'timelogs', TimeLogViewSet, basename='timelog')
+
+if DEBUG:
+    router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),

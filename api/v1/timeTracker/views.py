9b147a832bf272fc0e0b437defb0058102615908
Mohammad H. Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-08-20 05:13:16 +0430

    feat: implement ViewSets for User, TimeLog, Project

--- api/v1/timeTracker/views.py
+++ api/v1/timeTracker/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import Project, TimeLog
+from .serializers import ProjectSerializer, UserSerializer, TimeLogSerializer
 
-# Create your views here.
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        return Project.objects.filter(users=self.request.user)
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class TimeLogViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TimeLogSerializer
+
+    def get_queryset(self):
+        return TimeLog.objects.filter(project__users=self.request.user).filter(user=self.request.user)

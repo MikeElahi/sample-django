70f61bc5715a7fbd64d7e47ba35b60efd9f563a2
Mohammad H. Maazallahi <mohammadhoseinmazalahi@gmail.com>
2022-08-21 00:13:57 +0430

    feat: add registration to django admin

--- api/v1/timeTracker/admin.py
+++ api/v1/timeTracker/admin.py
@@ -1,3 +1,13 @@
+"""Admin Registrations for TimeTracker App"""
 from django.contrib import admin
+from .models import Project, TimeLog
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    """Project Admin Registrar Class"""
+
+
+@admin.register(TimeLog)
+class TimeLogAdmin(admin.ModelAdmin):
+    """TimeLog Model Admin Registrar Class"""
